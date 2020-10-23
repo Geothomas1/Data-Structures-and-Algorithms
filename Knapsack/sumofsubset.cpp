6f70a2457e89ae716ae49738007f56bb096b1e80
geothomas <geothomas@cet.ac.in>
2020-10-23 13:55:05 +0530

    sum of subset rec

--- Knapsack/sumofsubset.cpp
+++ Knapsack/sumofsubset.cpp
@@ -1,19 +1,43 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-bool sumofsubset(int arr[],int n,int sum)
+bool sumofsubset(int arr[],int sum, int n)
 {
+    int i,j;
+    bool subset[n+1][sum+1];
+      for (int i = 0; i <= n; i++) 
+        subset[i][0] = true; 
 
+    for (int i = 1; i <= sum; i++) 
+        subset[0][i] = false; 
 
-
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=sum;j++)
+        {
+            if(arr[i-1]<=sum)
+            {
+                subset[i][j]=subset[i-1][j] || subset[i-1][j-arr[i-1]];
+            }else{
+                subset[i][j]=subset[i-1][j];
+            }
+        }
+    }
+  return subset[n][sum];
 
 }
-
 int main()
 {
-    int arr[]={2,3,7,8,10};
-    int n=sizeof(arr)/arr[0];
-    int sum=11;
-    cout<<"Sum of Subset present"<<sumofsubset(arr,n,sum)<<endl;
+    int arr[]={3, 34, 4, 12, 5, 2};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int sum=10;
+    if(sumofsubset(arr,sum,n)==true)
+    {
+        cout<<"Subset is found in this array"<<endl;
+
+    }else
+    {
+        cout<<"Subset not found"<<endl;
+    }
+    
     return 0;
 }
